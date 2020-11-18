e36e46dc08ccd9ca5a23f2c04b7e573cf5416bbf
lu15v <lu15bytes@gmail.com>
2020-11-17 23:38:12 -0800

    FEATURE: added styles folder and first component header

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import { Container } from '@material-ui/core';
+
+import '../styles/header.css';
+
+const Header = () =>{
+
+    return(
+        <div className="hero-picture">
+            <div className="hero-text">
+                <h1>Webtry</h1>
+                <p>some fancy word over here</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
