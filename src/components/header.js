66959928e130db415147b07e45f8726df96e4aff
lu15v <lu15bytes@gmail.com>
2020-11-19 00:48:59 -0800

    STYLING: added for the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
-//import { Container } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
 
 import '../styles/header.css';
 
@@ -8,8 +10,21 @@ const Header = () =>{
     return(
         <div className="hero-picture">
             <div className="hero-text">
-                <h1>Webtry</h1>
-                <p>some fancy word over here</p>
+                <h1 className="main-title">
+                    <p className="title-letter-animation-1">W</p>
+                    <p className="title-letter-animation-2">e</p>
+                    <p className="title-letter-animation-3">b</p>
+                    <p className="title-letter-animation-4">t</p>
+                    <p className="title-letter-animation-5">r</p>
+                    <p className="title-letter-animation-6">y</p>
+                </h1>
+                <p className="slogan">Donde las palabras tocan el alma</p>
+                
+                <div className="arrow-icon">
+                    <a>
+                        <FontAwesomeIcon icon={faChevronDown} />
+                    </a>
+                </div>
             </div>
         </div>
     )
