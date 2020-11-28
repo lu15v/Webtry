a216cb57a3836c34c47a5526854a75e6dcb7893a
lu15v <lu15bytes@gmail.com>
2020-11-28 15:47:35 -0800

    FEATURE: poem-component created, still in dev

---
+++ src/components/poem-section.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PoemCard from './poem-card';
+
+import '../styles/poem-section.css';
+
+const PoemSection = props =>{
+
+
+    return(
+        <div className="hero container">
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+            <PoemCard/>
+        </div>
+    )
+}
+
+
+export default PoemSection;
