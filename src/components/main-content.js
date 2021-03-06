5518ead25feb6ea3f1892566de2c60d0e6db13b8
lu15v <lu15bytes@gmail.com>
2021-03-06 00:37:03 -0800

    FEATURE: drawable side menu implementation

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Icon } from "semantic-ui-react";
 
 import MenuBar from "./menu-bar";
 
+import '../styles/main-content.css';
 class MainContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: true };
+  }
+
   render() {
     return (
       <div className="container">
-        <MenuBar />
+        <MenuBar show={this.state.show} duration={500}/>
+        <div className="collapse-button fa-sm">
+          <Icon
+            name={this.state.show ? "angle left" : "angle right"}
+            size="big"
+            onClick={() => this.setState({show: !this.state.show})}
+          />
+        </div>
         {this.props.children}
       </div>
     );
--- src/components/menu-bar.js
+++ src/components/menu-bar.js
@@ -1,36 +1,52 @@
-import React, { useState } from "react";
-import { Menu } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Menu, Transition } from "semantic-ui-react";
 
 import "../styles/menu-bar.css";
 
-const MenuBar = () => {
+const MenuBar = ({show, duration}) => {
   const [activeItem, setActiveItem] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
+  // const [active, setActive] = useState(true);
 
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  console.log(width);
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
+  console.log(window.innerWidth);
   return (
-      <Menu fluid vertical tabular className="menu-container">
-        <Menu.Item
-          name="bio"
-          active={activeItem === "bio"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="pics"
-          active={activeItem === "pics"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="companies"
-          active={activeItem === "companies"}
-          onClick={handleItemClick}
-        />
-        <Menu.Item
-          name="links"
-          active={activeItem === "links"}
-          onClick={handleItemClick}
-        />
-      </Menu>
+      <Transition.Group animation={"fade right"} duration={duration}>
+        {show && (
+          <Menu fluid vertical tabular className="menu-container">
+            <Menu.Item
+              name="bio"
+              active={activeItem === "bio"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="pics"
+              active={activeItem === "pics"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="companies"
+              active={activeItem === "companies"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="links"
+              active={activeItem === "links"}
+              onClick={handleItemClick}
+            />
+          </Menu>
+        )}
+      {/* <Button onClick={() => setActive(!active)}>trigger</Button> */}
+      </Transition.Group>
   );
 };
 
